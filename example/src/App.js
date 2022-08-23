1b01259c7bf004b14ad7f11fdaa735ac854a72bf
kaichi <hellokitty321ds@gmail.com>
2022-08-23 19:31:36 +0800

    Init

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-import { useMyHook } from 'use-detect-overflow'
+import { useDetectOverflow } from 'use-detect-overflow';
 
 const App = () => {
-  const example = useMyHook()
+  const [ref, { overflowX, overflowY }] = useDetectOverflow();
+
+  console.log(overflowX, overflowY);
+
   return (
-    <div>
-      {example}
+    <div ref={ref} style={{ height: 200 }}>
+      Consectetur exercitation magna occaecat mollit adipisicing minim do irure
+      laboris nostrud. Eu adipisicing do do exercitation eiusmod cupidatat
+      officia occaecat sit Lorem aute enim ipsum laborum. Qui commodo cupidatat
+      et voluptate in sit ut exercitation commodo sint. Enim amet pariatur irure
+      ad magna quis labore veniam veniam officia. Aliquip ut occaecat non Lorem.
+      Laboris eu magna duis nostrud incididunt irure. Incididunt ad enim commodo
+      culpa. Adipisicing tempor et irure dolore Lorem magna ut cupidatat veniam.
+      Ut quis amet aliqua non velit eiusmod ullamco elit ex pariatur laborum.
+      Ullamco non ullamco aliquip voluptate Lorem elit incididunt non nostrud
+      aliquip. Mollit id commodo amet ut minim pariatur consequat ut eiusmod do
+      enim ut. Nostrud consectetur aute cupidatat est consequat. Sint duis quis
+      deserunt cupidatat dolore exercitation ullamco. Ad qui quis laboris in
+      qui. Dolor dolor aliquip qui aute non ea aliquip anim enim magna. Id sint
+      voluptate elit voluptate consequat. Dolore exercitation esse consequat
+      nulla et mollit labore nostrud. Ipsum adipisicing voluptate incididunt
+      nulla id eiusmod. Eu velit cillum in est quis aliquip nostrud ea mollit
+      culpa. Cillum dolor sit exercitation nisi deserunt. Cillum amet nostrud
+      esse quis aute anim ad labore in aute. Adipisicing do sunt qui pariatur ex
+      enim commodo elit consequat amet mollit cillum culpa. Nostrud amet Lorem
+      non elit occaecat cillum quis. Exercitation reprehenderit culpa sint
+      laborum do elit. Dolore laborum nulla exercitation laborum.
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,57 @@
-import * as React from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { ResizeObserver } from '@juggle/resize-observer';
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
+type HTMLOrSVGElement = HTMLElement | SVGElement;
+
+export type State = {
+  element: HTMLOrSVGElement | null;
+  resizeObserver: ResizeObserver | null;
+};
+
+export function useDetectOverflow() {
+  const state = useRef<State>({ element: null, resizeObserver: null });
+  const [overflowX] = useState(false);
+  const [overflowY] = useState(false);
+  const mounted = useRef(false);
+
+  useEffect(() => {
+    mounted.current = true;
+    return () => void (mounted.current = false);
   });
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+  const ref = (node: HTMLOrSVGElement | null) => {
+    if (!node || state.current.element === node) return;
 
-  return counter;
-};
+    state.current.element = node;
+  };
+
+  const handleResize = (
+    entries: ResizeObserverEntry[],
+    observer: ResizeObserver
+  ) => {
+    console.log('resize', entries);
+  };
+
+  function removeListeners() {
+    if (state.current.resizeObserver) {
+      state.current.resizeObserver.disconnect();
+      state.current.resizeObserver = null;
+    }
+  }
+
+  function addListeners() {
+    if (!state.current.element) return;
+    state.current.resizeObserver = new ResizeObserver(handleResize);
+    state.current.resizeObserver.observe(state.current.element);
+  }
+
+  useEffect(() => {
+    if (!mounted) return;
+
+    addListeners();
+
+    return () => removeListeners();
+  }, [state.current.element]);
+
+  return [ref, { overflowX, overflowY }] as const;
+}
