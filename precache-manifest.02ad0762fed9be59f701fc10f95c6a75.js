55103b5c5bab63b061b0f16393555a73698d74db
kaichi <hellokitty321ds@gmail.com>
2022-08-23 23:37:28 +0800

    Updates

---
+++ precache-manifest.02ad0762fed9be59f701fc10f95c6a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02edf788dcfadab4c6b551605927f7d7",
+    "url": "/use-detect-overflow/index.html"
+  },
+  {
+    "revision": "db96561560e880b1efe3",
+    "url": "/use-detect-overflow/static/css/main.77e1d308.chunk.css"
+  },
+  {
+    "revision": "149eb412d9b7f1e21427",
+    "url": "/use-detect-overflow/static/js/2.b40837c1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-detect-overflow/static/js/2.b40837c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db96561560e880b1efe3",
+    "url": "/use-detect-overflow/static/js/main.2d609ab1.chunk.js"
+  },
+  {
+    "revision": "aa9b2fec543c7eb20ecd",
+    "url": "/use-detect-overflow/static/js/runtime-main.dc8e488a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/use-detect-overflow/precache-manifest.02ad0762fed9be59f701fc10f95c6a75.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/use-detect-overflow/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
